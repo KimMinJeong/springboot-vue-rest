3bff6ba74cb8e9f6ee1edbb3fdc0b5e1689b4971
KimMinJeong <sinred12345@naver.com>
2019-02-18 17:39:22 +0900

    version 0.0.1 - NewsView Styling

--- frontend/src/views/NewsView.vue
+++ frontend/src/views/NewsView.vue
@@ -1,11 +1,30 @@
 <template>
   <div>
-    <div v-for="item in this.$store.state.news" :key="item.title">
+    <ul class="news-list">
+      <li v-for="item in this.$store.state.news" class="post" v-bind:key="item">
+        <!-- 포인트 영역 -->
+        <div class="points">
+          {{ item.points }}
+        </div>
+        <!-- 기타 정보 영역 -->
+        <div>
+          <p class="news-title">
+            <a v-bind:href="item.url">
+              {{ item.title }}
+            </a>
+          </p>
+          <small class="link-text">
+            by <router-link v-bind:to="`/user/${item.user}`">{{ item.user }}</router-link>
+          </small>
+        </div>
+      </li>
+    </ul>
+    <!-- <div v-for="item in this.$store.state.news" :key="item.title">
       <a v-bind:href="item.url">{{ item.title }}</a>
       <small>{{ item.time_ago }} by
         <router-link v-bind:to="'/user/' + item.user">{{ item.user }}</router-link>
       </small>
-    </div>
+    </div> -->
   </div>
 </template>
 
@@ -17,5 +36,29 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+  .news-list {
+    margin: 0px;
+    padding: 0px;
+  }
+  .post {
+    display: flex;
+    list-style: none;
+    align-items: center;
+    boder-bottom: 1px solid #eee;
+  }
+  .points {
+    width: 80px;
+    heigth: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    color: #42b883;
+  }
+  .news-title {
+    margin: 0;
+  }
+  .link-text {
+
+  }
 </style>

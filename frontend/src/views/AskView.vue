6c197e7005b9acdf1f1c5315ef3b923510c0c7c0
KimMinJeong <sinred12345@naver.com>
2019-02-18 14:32:33 +0900

    version 0.0.1 - Module store - actions, mutations

--- frontend/src/views/AskView.vue
+++ frontend/src/views/AskView.vue
@@ -1,11 +1,28 @@
 <template>
   <div>
-    <div v-for="p in this.$store.state.ask" :key="p.title">{{ p.title }}</div>
+    <div v-for="p in itemAsk" :key="p.title">{{ p.title }}</div>
   </div>
 </template>
 
 <script>
+import { mapGetters } from 'vuex'
+
 export default {
+  computed: {
+    // #3
+    ...mapGetters({
+      itemAsk: 'fetchedAsk'
+    })
+    // #2
+    // ...mapState({
+    //  fechedAsk: state => state.ask
+    // })
+
+    // #1
+    // ask () {
+    //  return this.$store.state.ask()
+    // }
+  },
   created () {
     this.$store.dispatch('FETCH_ASK')
   }
